9040ef1a8a7ba80d4e7fd586d41b85ddace2e6bb
Arseni20032 <teplyakovarseni@mail.ru>
2023-03-31 11:11:43 +0300

    added avg_length_of_word

---
+++ lab_2/avg_lenght_of_word.py
@@ -0,0 +1,11 @@
+import re
+
+
+def avg_length_of_word(sentences: list):
+    numbers_of_words = 0
+    sum_of_characters = 0
+    for sentence in sentences:
+        numbers_of_words += len(sentence.split(''))
+        chars = re.findall(r"[A-Za-z]", sentence)
+        sum_of_characters += len(chars)
+    return int(sum_of_characters / numbers_of_words)
