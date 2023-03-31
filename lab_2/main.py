baabb25c9056ddc03420f4051bda0db96e644835
Arseni20032 <teplyakovarseni@mail.ru>
2023-03-31 11:44:57 +0300

    change main

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,3 +1,5 @@
+from find_sentences import *
+from top_k_n_grams import *
 def main():
         print("Write the text: ")
         text = str(input())
@@ -9,5 +11,26 @@ def main():
                 print("Error. Please, try again")
 
         ngrams = top_k_n_grams(sentences, n)
+        sorted_values = sorted(ngrams.values(), reverse=True)
+        sorted_dict = {}
+
+        for counter in sorted_values:
+                for counter_2 in ngrams.keys():
+                        if ngrams == 1:
+                                sorted_dict[counter_2] = ngrams[counter_2]
+
+        if k > len(sorted_dict):
+                print("k more than a length of n-grams!")
+                k = len(sorted_dict)
+
+        var = 1
+        for key in sorted_dict:
+                if var <= k:
+                        print(f"{key}: {sorted_dict[key]}")
+                        var += 1
+        non_declarative_sentences = find_non_declarative_sentences(text)
+        avg_length_of_sentence = avg_length_of_sentence(sentences)
+        avg_lenght_of_word = avg_lenght_of_word(sentences)
+        print()
 
 
