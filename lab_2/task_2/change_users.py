5af3461a08c78b85627b679936b310df639cd208
Arseni20032 <teplyakovarseni@mail.ru>
2023-04-01 23:12:51 +0300

    change change_users functions

--- lab_2/task_2/change_users.py
+++ lab_2/task_2/change_users.py
@@ -1,17 +1,17 @@
 def change_users(self, name: str):
-    if self._current_user is not None:
+    if self._curr_user is not None:
         text = input("Save current container? Enter YES or NO")
         if text == "YES" or "Yes" or "yes":
             self.save()
         else:
             self._cont[name] = set()
 
-    self._current_user = name
+    self._curr_user = name
 
-    if not self._current_user in self._cont.keys():
-        self._cont[self._current_user] = set()
+    if not self._curr_user in self._cont.keys():
+        self._cont[self._curr_user] = set()
 
-    print(f"Switched to {self._current_user}")
+    print(f"Switched to {self._curr_user}")
 
     text1 = input("Load the container? Enter YES or NO")
     if text1 == "YES" or "Yes" or "yes":
