b6fc206e4cc33301dfc165e78a0d655d7f282401
Arseni20032 <teplyakovarseni@mail.ru>
2023-04-01 23:30:59 +0300

    added find function

---
+++ lab_2/task_2/find.py
@@ -0,0 +1,6 @@
+def find(self, *objs):
+    if self._curr_user is not None:
+        for obj in objs:
+            if obj in self._cont[self._curr_user]:
+                print(f"founded: {obj}")
+                continue
