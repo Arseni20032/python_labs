b933e58c602d572815244697b244df677aef8b45
Arseni20032 <teplyakovarseni@mail.ru>
2023-04-01 13:38:53 +0300

    added input check

--- lab_2/find_sentences.py
+++ lab_2/find_sentences.py
@@ -1,5 +1,7 @@
 import re
 from constants import *
+
+
 def find_sentences(text: str):
     regexpr = f"((({mister_or_mrs_reg}|{jr_reg}|{etc_reg}|{eg_reg}|{c_reg}|{ie_reg}|{initials_reg})|{not_punct_reg})*{punc_reg})"
     sentences_with_captured_groups = re.findall(regexpr, text)
--- lab_2/main.py
+++ lab_2/main.py
@@ -1,36 +1,66 @@
+import find_non_declarative_sentences
+from avg_length_of_sentence import avg_length_of_sentence
+from avg_length_of_word import avg_length_of_word
 from find_sentences import *
 from top_k_n_grams import *
+
+
 def main():
+    while True:
         print("Write the text: ")
         text = str(input())
         print("Write n and k ( > 0 ): ")
-        n = int(input())
-        k = int(input())
+        while True:
+            try:
+                n = int(input())
+                k = int(input())
+            except ValueError:
+                print("Error. Please, try again")
+                continue
+            if n <= 0 or k <= 0:
+                print("Error. Please, try again (n or k <= 0) ")
+                continue
+            break
         sentences = find_sentences(text)
         if len(sentences) == 0:
-                print("Error. Please, try again")
+            print("Error. Please, try again (len of sentences = 0)")
+            continue
 
         ngrams = top_k_n_grams(sentences, n)
         sorted_values = sorted(ngrams.values(), reverse=True)
         sorted_dict = {}
 
         for counter in sorted_values:
-                for counter_2 in ngrams.keys():
-                        if ngrams == 1:
-                                sorted_dict[counter_2] = ngrams[counter_2]
+            for counter_2 in ngrams.keys():
+                if ngrams[counter_2] == counter:
+                    sorted_dict[counter_2] = ngrams[counter_2]
 
         if k > len(sorted_dict):
-                print("k more than a length of n-grams!")
-                k = len(sorted_dict)
+            print("k more than a length of n-grams!")
+            k = len(sorted_dict)
 
         var = 1
         for key in sorted_dict:
-                if var <= k:
-                        print(f"{key}: {sorted_dict[key]}")
-                        var += 1
-        non_declarative_sentences = find_non_declarative_sentences(text)
-        avg_length_of_sentence = avg_length_of_sentence(sentences)
-        avg_lenght_of_word = avg_lenght_of_word(sentences)
-        print()
+            if var <= k:
+                print(f"{key}: {sorted_dict[key]}")
+                var += 1
+
+        non_declarative_sentences = find_non_declarative_sentences.find_non_declarative_sentences(text)
+        avg_length_of_sentence1 = avg_length_of_sentence(sentences)
+        avg_length_of_word1 = avg_length_of_word(sentences)
+        print(f"List of sentences: \n {sentences}")
+        print(f"list of non declarative sentences: \n {non_declarative_sentences}")
+        print(f"average number of characters in sentence: {avg_length_of_sentence}")
+        print(f"average number of characters in word: \n {avg_length_of_word} \n")
+        print("Do you want to continue? Print YES or NO")
+        decision = input()
+        if decision == "YES" or "yes" or "Yes":
+            continue
+        else:
+            break
 
 
+if __name__ == "__main__":
+    main()
+
+# Hello. It's a testing text! What the weather like today?
