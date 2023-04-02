3a781b72bddeb2ef1c9ab94b153f5405468bf45d
Arseni20032 <teplyakovarseni@mail.ru>
2023-04-02 15:28:26 +0300

    added menu for 2 task

--- lab_2/task_1/top_k_n_grams.py
+++ lab_2/task_1/top_k_n_grams.py
@@ -7,12 +7,11 @@ def top_k_n_grams(sentences: list, n: int):
         words = re.findall(r"[A-za-z0-9]+", sentence)
         if len(words) >= n:
             for counter in range(0, len(words) - n + 1):
-                str = words[counter]
+                str1 = words[counter]
                 for counter_2 in range(counter + 1, counter + n):
-                    str += '' + words[counter_2]
-                list_of_grams.append(str)
+                    str1 += '' + words[counter_2]
+                list_of_grams.append(str1)
     dict_of_grams = dict()
-    counter = 1
     for gram in list_of_grams:
         dict_of_grams[gram] = list_of_grams.count(gram)
     return dict_of_grams
--- lab_2/task_2/main.py
+++ lab_2/task_2/main.py
@@ -1,3 +1,63 @@
+
+class Container:
+    from remove import remove
+    from add_users import add_users
+    from load import load
+    from add import add
+    from find import find
+    from print_fun import print_fun
+    from grep import grep
+    from save import save
+    from change_users import change_users
+    _cont = dict()
+    _curr_user = None
+
+
 def main():
-    print("Enter your username to make possible to load your last container")
+    _cont = Container()
+
+    print("Enter your username to make possible to load your last container: ")
     name = input()
+    _cont.add_users(name)
+    _cont.load()
+
+    choose_menu = "Menu:\n 1. Add element\n 2. Remove\n 3. Find key\n 4. Print list\n 5. Grep\n" \
+                  " 6. Save\n 7. Load\n 8. Switch user\n 9. Exit\n"
+
+    while True:
+        print(choose_menu)
+        decision = input()
+
+        if decision == "1":
+            print("Enter the element: ")
+            _cont.add(input())
+        elif decision == "2":
+            print("Enter the element: ")
+            _cont.remove(input())
+        elif decision == "3":
+            print("Enter the element: ")
+            _cont.find(input())
+        elif decision == "4":
+            print("Printing...")
+            _cont.print_fun()
+        elif decision == "5":
+            print("Enter regex to match: ")
+            _cont.grep(input())
+        elif decision == "6":
+            print("Saving...")
+            _cont.save()
+        elif decision == "7":
+            print("Load...")
+            _cont.load()
+        elif decision == "8":
+            print("Enter the name of user to switch: ")
+            _cont.change_users(input())
+        elif decision == "9":
+            break
+        else:
+            print("Error! Please, try again!\n")
+            continue
+
+
+if __name__ == "__main__":
+    main()
